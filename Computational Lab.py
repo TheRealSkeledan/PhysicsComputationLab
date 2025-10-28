83672db2a37fab4756cd2175984df671445b7260
Skeledan <121405337+TheRealSkeledan@users.noreply.github.com>
2025-10-28 10:17:29 -0400

    Update Computational Lab.py

--- Computational Lab.py
+++ Computational Lab.py
@@ -1,6 +1,7 @@
 import math
 import matplotlib.pyplot as plt
 
+# Initializing variables
 t = 0.0
 x = 0.0
 y = 0.0
@@ -16,11 +17,13 @@ x_values = []
 y_values = []
 t_values = []
 
+# Some math thing
 v0x = v0*math.cos(math.radians(angle))
 v0y = v0*math.sin(math.radians(angle))
 print("v0x = ", v0x)
 print("v0y = ", v0y)
 
+# Putting everything into the array
 while y>=0:
   x = v0x*t + (0.5)*ax*t*t
   y = v0y*t +(0.5)*ay*t*t
@@ -32,18 +35,21 @@ while y>=0:
 
 plt.figure(figsize=(12,4))
 
+# y vs x graph
 plt.subplot(1,3,1)
 plt.plot(x_values, y_values)
 plt.title("Projectile Path (y vs x)")
 plt.xlabel("x-position (m)")
 plt.ylabel("y-position (m)")
 
+# x vs time graph
 plt.subplot(1,3,2)
 plt.plot(t_values, x_values)
 plt.title("x-position vs Time")
 plt.xlabel("Time (s)")
 plt.ylabel("x-position (m)")
 
+# y vs time graph
 plt.subplot(1,3,3)
 plt.plot(t_values, y_values)
 plt.title("y-position vs Time")
@@ -57,12 +63,7 @@ range_x = x_values[-1]
 time_in_air = t_values[-1]
 max_height = max(y_values)
 
+# I hope this works
 print(f"\nPredicted Range (m): {range_x:.2f}")
 print(f"Predicted Time in Air (s): {time_in_air:.2f}")
 print(f"Predicted Max Height (m): {max_height:.2f}")
-
-# Example:
-experimental_range = float(input("Measured (experimental) range (m): "))
-
-percent_error = abs((experimental_range - range_x) / range_x) * 100
-print(f"Percent error: {percent_error:.2f}%")
